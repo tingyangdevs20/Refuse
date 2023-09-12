7ce47678bd0ad36a826d209f4f54d8e8d723b5d9
AleksandarBrankovic98 <brankovical98@gmail.com>
2023-09-13 03:33:21 +0530

    setup

--- resources/views/back/index.blade.php
+++ resources/views/back/index.blade.php
@@ -14,7 +14,7 @@
 
                         <div class="page-title-right">
                             <ol class="breadcrumb m-0">
-                                <li class="breadcrumb-item"><a href="#">Command Central</a></li>
+                                <li class="breadcrumb-item"><a href="#">Command Centralll</a></li>
                             </ol>
                         </div>
 
