132fa7bda890f50757dc7905f7edacc8ce66a0e7
AleksandarBrankovic98 <brankovical98@gmail.com>
2023-09-14 01:31:10 +0530

    migrations

--- resources/views/back/index.blade.php
+++ resources/views/back/index.blade.php
@@ -36,7 +36,7 @@
                                         </div>
                                         <div class="media-body align-self-center">
                                             <div class="text-muted">
-                                                <p class="mb-2">Welcomeeee</p>
+                                                <p class="mb-2">Welcome</p>
                                                 <h5 class="mb-1">{{ Auth::user()->name }}</h5>
                                             </div>
                                         </div>
