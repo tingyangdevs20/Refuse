f7b65d48e7b51e6da89c76025b1557bd87ac9917
AleksandarBrankovic98 <brankovical98@gmail.com>
2023-09-19 12:24:23 -0700

    updates

---
+++ vendor/dacastro4/laravel-gmail/src/LaravelGmailServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Dacastro4\LaravelGmail;
+
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\ServiceProvider;
+
+class LaravelGmailServiceProvider extends ServiceProvider
+{
+
+	public function boot()
+	{
+		$this->publishes([__DIR__.'/config/gmail.php' => App::make('path.config').'/gmail.php',]);
+	}
+
+	public function register()
+	{
+
+		$this->mergeConfigFrom(__DIR__.'/config/gmail.php', 'gmail');
+
+		// Main Service
+		$this->app->bind('laravelgmail', function ($app) {
+			return new LaravelGmailClass($app['config']);
+		});
+
+	}
+}
