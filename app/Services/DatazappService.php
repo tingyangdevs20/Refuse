7a5b4cef9d245186377d1123a0d9c2734eb69f07
AleksandarBrankovic98 <brankovical98@gmail.com>
2023-09-19 20:21:41 +0530

    reverting

--- app/Services/DatazappService.php
+++ app/Services/DatazappService.php
@@ -17,153 +17,43 @@ class DatazappService
         $this->apiKey = config('services.datazapp.api_key');
     }
 
-    public function skipTrace($contacts, $skipTraceOption)
+    public function skipTrace($contacts)
     {
-
-
         $client = new Client();
 
-        // Filter contacts based on the selected skip trace option
-        $contactsToSkipTrace = collect([]);
-
-        if ($skipTraceOption === 'skip_entire_list_phone') {
-            $contactsToSkipTrace = $contacts;
-        } elseif ($skipTraceOption === 'skip_records_without_numbers_phone') {
-            $contactsToSkipTrace = $contacts;
-
-        } elseif ($skipTraceOption === 'skip_entire_list_email') {
-            $contactsToSkipTrace = $contacts;
-        } elseif ($skipTraceOption === 'skip_records_without_emails') {
-            $contactsToSkipTrace = $contacts;
-        } elseif ($skipTraceOption === 'append_names'){
-            $contactsToSkipTrace = $contacts;
-        } elseif ($skipTraceOption === 'email_verification_entire_list'){
-            $contactsToSkipTrace = $contacts->where('email1','!=', '');
-        } elseif ($skipTraceOption === 'email_verification_non_verified'){
-            $contactsToSkipTrace = $contacts->where('email1','!=', '');
-        } elseif ($skipTraceOption === 'phone_scrub_entire_list'){
-            $contactsToSkipTrace = $contacts;
-        } elseif ($skipTraceOption === 'phone_scrub_non_scrubbed_numbers'){
-            $contactsToSkipTrace = $contacts;
-        } elseif ($skipTraceOption === 'append_emails'){
-            $contactsToSkipTrace = $contacts;
-        } else {
-            // Handle other skip trace options if needed
-            return ['message' => 'Invalid skip trace option.'];
-        }
+        // Filter contacts to skip trace only those with phone numbers
+        $contactsToSkipTrace = $contacts->filter(function ($contact) {
+            return !empty($contact->number);
+        });
 
         if ($contactsToSkipTrace->isEmpty()) {
-            return ['message' => 'No contacts matching the selected skip trace option.'];
+            return ['message' => 'No contacts with phone numbers to skip trace.'];
         }
 
-        // Prepare the request data based on the selected skip trace option
+        // Prepare the request data based on the Datazapp API example
         $requestData = [
-            "ApiKey" => "IFFVVZBJTO", // Replace with your API key
+            "ApiKey" => "IFFVVZBJTO",
+            "AppendModule" => "EmailAppend",
+            "AppendType" => 1, // 1 for Individual
             "Data" => [],
         ];
 
         foreach ($contactsToSkipTrace as $contact) {
-            if ($skipTraceOption === 'skip_entire_list_phone' || $skipTraceOption === 'skip_records_without_numbers_phone') {
-                // Phone Append API request
-
-                $requestData['AppendModule'] = "PhoneAppendAPI";
-                $requestData['AppendType'] = 2; // 2 for Landline
-                $requestData['DncFlag'] = "true"; // Set DNC flag if needed
-
-                $requestData['Data'][] = [
-                    "FirstName" => $contact->name,
-                    "LastName" => $contact->last_name,
-                    "Address" => $contact->street,
-                    "City" => $contact->city,
-                    "Zip" => $contact->zip,
-
-
-                    // Add other required parameters for phone append
-                ];
-
-            } elseif ($skipTraceOption === 'skip_entire_list_email' || $skipTraceOption === 'skip_records_without_emails') {
-                // Email Append API request
-                $requestData['AppendModule'] = "EmailAppend";
-                $requestData['AppendType'] = 1; // 1 for Individual
-
-                $requestData['Data'][] = [
-                    "FirstName" => $contact->name,
-                    "LastName" => $contact->last_name,
-                    "Address" => $contact->street,
-                    "City" => $contact->city,
-                    "Zip" => $contact->zip,
-
-                    // Add other required parameters for email append
-                ];
-            }elseif($skipTraceOption === 'append_names'){
-                // Name Append API request
-                $requestData['AppendModule'] = "NameAppendAPI";
-
-
-                $requestData['Data'][] = [
-
-                    "Address" => $contact->street,
-                    "City" => $contact->city,
-                    "Zip" => $contact->zip,
-
-                    // Add other required parameters for email append
-                ];
-            }
-            elseif($skipTraceOption === 'append_emails'){
-                // Name Append API request
-                $requestData['AppendModule'] = "EmailAppend";
-                $requestData['AppendType'] = 1; // 1 for Individual
-
-                $requestData['Data'][] = [
-                    "FirstName" => $contact->name,
-                    "LastName" => $contact->last_name,
-                    "Address" => $contact->street,
-                    "City" => $contact->city,
-                    "Zip" => $contact->zip,
-
-                    // Add other required parameters for email append
-                ];
-
-            }elseif($skipTraceOption === 'email_verification_entire_list' || $skipTraceOption === 'email_verification_non_verified'){
-                 // Emal Verificationd API request
-                 $requestData['AppendModule'] = "EmailVerificationAPI";
-
-                 $requestData['Data'][] = [
-
-                     "Email" => $contact->email1,
-                     "Email" => $contact->email2,
-
-
-                     // Add other required parameters for email append
-                 ];
-            } elseif($skipTraceOption === 'phone_scrub_entire_list' || $skipTraceOption === 'phone_scrub_non_scrubbed_numbers'){
-                 // phone scrubbing API request
-                 $requestData['AppendModule'] = "PhoneScrubAPI";
-
-                 $requestData['Data'][] = [
-
-                     "Phone" => $contact->number,
-                     "Phone" => $contact->number2,
-                     "Phone" => $contact->number3,
-
-
-                     // Add other required parameters for email append
-                 ];
-            }else {
-                // Handle other skip trace options if needed
-                return ['message' => 'Invalid skip trace option.'];
-            }
+            $requestData['Data'][] = [
+                "FirstName" => $contact->name,
+                "LastName" => $contact->last_name,
+                "Address" => $contact->street,
+                "City" => $contact->city,
+                "Zip" => $contact->zip,
+            ];
         }
-
 
         $response = $client->post('https://secureapi.datazapp.com/Appendv2', [
             'json' => $requestData,
         ]);
 
-
         return json_decode($response->getBody(), true);
     }
-
 
 
 }
