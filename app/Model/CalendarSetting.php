8960bd9f26fe9426cc1b7bb2365680694245c968
AleksandarBrankovic98 <brankovical98@gmail.com>
2023-10-02 00:54:21 +0500

    Implemented functionality to let the admin setup his appointments availibility from administrative settings. and then on the basis of those settings the user will be able see the timeslots in the calendar while booking an appointment. Plus user can book and see appointments in his own timezone.

---
+++ app/Model/CalendarSetting.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CalendarSetting extends Model
+{
+    protected $table = "calendar_settings";
+    protected $guarded = [];
+}
