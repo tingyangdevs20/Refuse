c66b77fdc5caee86f792d5cebd5b7701333dc516
AleksandarBrankovic98 <brankovical98@gmail.com>
2023-10-10 20:42:22 +0530

    contact details

--- resources/views/back/pages/group/contactDetail.blade.php
+++ resources/views/back/pages/group/contactDetail.blade.php
@@ -4895,7 +4895,7 @@
                                                                         <label>Template Content <span
                                                                                 class="required">*</span></label>
                                                                         <textarea class="form-control text1 userAgreementContent" id="user-agreement-content" name="content"
-                                                                            rows="10"></textarea>
+                                                                            rows="10" ></textarea>
                                                                         <div id='count' class="float-lg-right">
                                                                         </div>
                                                                     </div>
