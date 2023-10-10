7ee14dc46acc1cdf36e3b0174711851b84bff5ed
AleksandarBrankovic98 <brankovical98@gmail.com>
2023-10-10 17:13:10 +0530

    lists

--- resources/views/back/pages/group/view_all.blade.php
+++ resources/views/back/pages/group/view_all.blade.php
@@ -14,12 +14,12 @@
             <div class="row">
                 <div class="col-12">
                     <div class="page-title-box d-flex align-items-center justify-content-between">
-                        <h4 class="mb-0 font-size-18">Group Management</h4>
+                        <h4 class="mb-0 font-size-18">Lists Management</h4>
                         <div class="page-title-right">
                             <ol class="breadcrumb m-0">
                                 <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Dashboard</a></li>
-                                <li class="breadcrumb-item">Group Management</li>
-                                <li class="breadcrumb-item active">Numbers</li>
+                                <li class="breadcrumb-item">Lead Generation</li>
+                                <li class="breadcrumb-item active">My Lists</li>
                             </ol>
                         </div>
                     </div>
