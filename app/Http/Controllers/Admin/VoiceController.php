c7dbdb9a1b3aa0683c0c93a9a3cea1410c2be0ae
AleksandarBrankovic98 <brankovical98@gmail.com>
2023-10-24 20:04:41 +0500

    commit new testing

--- app/Http/Controllers/Admin/VoiceController.php
+++ app/Http/Controllers/Admin/VoiceController.php
@@ -24,8 +24,6 @@ class VoiceController extends Controller
         $settings = Settings::first()->toArray();
         $callerId = $settings['call_forward_number'];
         $response = new VoiceResponse();
-        $response->say('Hello, friend. This is a test call from Twilio make by REIFuze!');
-        return response($response)->header('Content-Type', 'application/xml');
         if ($phone == $callerId) {
             # Receiving an incoming call to the browser from an external phone
             $dial = $response->dial($phone);
@@ -36,7 +34,7 @@ class VoiceController extends Controller
 
             // Only include the necessary elements in the TwiML
             if (preg_match("/^[\d\+\-\(\) ]+$/", $number)) {
-                $dial->number($number);
+                $dial->number($number, ['token' => $this->generateAccessTokenforIncomingCall()]);
             } else {
                 $dial->client($number);
             }
@@ -80,5 +78,38 @@ class VoiceController extends Controller
 
         $token = $accessToken->toJWT();
         return response()->json(['token' => $token]);
+    }
+
+    public function generateAccessTokenforIncomingCall()
+    {
+
+        $settings = Settings::first()->toArray();
+
+
+        //old account 
+
+        //$TWILIO_ACCOUNT_SID = 'ACa068bcfb703b21e18077f86851761d44';
+        // $TWILIO_SECRET_KEY = 'ev637SpAE8pP16xKI8wkuToVGrDtlkwt';
+        // $API_KEY = 'SKe98914905647ed119d608121a51534db';
+        // $TWIML_APP_SID = 'AP9150882055bff4025c1f7c6d94925d7d';
+
+
+        //new account 
+
+        $TWILIO_ACCOUNT_SID = 'ACa068bcfb703b21e18077f86851761d44';
+        $TWILIO_SECRET_KEY = 'ev637SpAE8pP16xKI8wkuToVGrDtlkwt';
+        $API_KEY = 'SKe98914905647ed119d608121a51534db';
+        $TWIML_APP_SID = 'AP9150882055bff4025c1f7c6d94925d7d';
+
+        $accessToken = new AccessToken($TWILIO_ACCOUNT_SID, $API_KEY, $TWILIO_SECRET_KEY, 3600, 'bulk-sms');
+
+
+        $voiceGrant = new VoiceGrant();
+        $voiceGrant->setOutgoingApplicationSid($TWIML_APP_SID);
+        $voiceGrant->setIncomingAllow(true);
+        $accessToken->addGrant($voiceGrant);
+
+        $token = $accessToken->toJWT();
+        return $token;
     }
 }
