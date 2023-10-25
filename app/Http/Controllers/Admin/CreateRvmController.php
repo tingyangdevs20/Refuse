b38fc7c0930a0d000617fd65e90b46ba11c4e319
AleksandarBrankovic98 <brankovical98@gmail.com>
2023-10-25 23:19:19 +0530

    live url

--- app/Http/Controllers/Admin/CreateRvmController.php
+++ app/Http/Controllers/Admin/CreateRvmController.php
@@ -46,7 +46,7 @@ class CreateRvmController extends Controller
             $extension = $media->getClientOriginalExtension();
             $tmpname = 'RVM_'.time() .'.'. $extension;
             $path = $media->storeAs("MMS_Media", $tmpname, "uploads");
-            $media = config('app.url') . '/public/uploads/' . $path;
+            $media = config('app.live_url') . '/public/uploads/' . $path;
         }
         $rvm = new RvmFile();
         $rvm->name = $request->name;
