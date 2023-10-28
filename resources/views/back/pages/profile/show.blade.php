144b6ccbbb138192c51b11f14525fd244f4887b2
AleksandarBrankovic98 <brankovical98@gmail.com>
2023-10-28 23:50:36 +0530

    profile

--- resources/views/back/pages/profile/show.blade.php
+++ resources/views/back/pages/profile/show.blade.php
@@ -65,20 +65,26 @@
                                             <div class="text-danger">{{ $message }}</div>
                                         @enderror
                                     </div>
+                                    <div class="row">
+                                        
 
-                                    <div class="form-group">
-                                        <label for="password">New Password</label>
-                                        <input type="password" name="password" id="password" class="form-control">
-                                        @error('password')
-                                            <div class="text-danger">{{ $message }}</div>
-                                        @enderror
+                                        <div class="col">
+                                            <label for="name">New Password</label>
+                                            <input type="password" name="password" id="password" class="form-control"
+                                               >
+                                            @error('password')
+                                                <div class="text-danger">{{ $message }}</div>
+                                            @enderror
+                                        </div>
+                                        <div class="col">
+                                            <label for="password_confirmation">Confirm New Password</label>
+                                            <input type="password" name="password_confirmation" id="password_confirmation" class="form-control"
+                                                >
+                                            
+                                        </div>
+                                        
                                     </div>
-
-                                    <div class="form-group">
-                                        <label for="password_confirmation">Confirm New Password</label>
-                                        <input type="password" name="password_confirmation" id="password_confirmation"
-                                            class="form-control">
-                                    </div>
+                                   <br/>
 
                                     <div class="form-group">
                                         <label for="company_name">Company Name</label>
@@ -89,25 +95,29 @@
                                         @enderror
                                     </div>
 
-                                    <div class="form-group">
-                                        <label for="address">Address</label>
-                                        <input type="text" name="address" id="address" class="form-control"
+                                   
+                                    <div class="row">
+                                        
+
+                                        <div class="col">
+                                            <label for="address">Address</label>
+                                            <input type="text" name="address" id="address" class="form-control"
                                             value="{{ old('address', $user->address) }}">
-                                        @error('address')
-                                            <div class="text-danger">{{ $message }}</div>
-                                        @enderror
+                                            @error('address')
+                                                <div class="text-danger">{{ $message }}</div>
+                                            @enderror
+                                        </div>
+                                        <div class="col">
+                                            <label for="address2">Address 02 (Optional)</label>
+                                            <input type="text" name="address2" id="address2" class="form-control"
+                                            value="{{ old('address2', $user->address2) }}" >
+                                               
+                                            
+                                        </div>
+                                        
                                     </div>
 
-                                    <div class="form-group">
-                                        <label for="address">Address 02 (Optional)</label>
-                                        <input type="text" name="address2" id="address2" class="form-control"
-                                            value="{{ old('address2', $user->address2) }}">
-                                        @error('address2')
-                                            <div class="text-danger">{{ $message }}</div>
-                                        @enderror
-                                    </div>
-
-                                    <div class="form-group">
+                                    <div style="display:none" class="form-group">
                                         <label for="street">Street</label>
                                         <input type="text" name="street" id="street" class="form-control"
                                             value="{{ old('street', $user->street) }}">
@@ -115,36 +125,36 @@
                                             <div class="text-danger">{{ $message }}</div>
                                         @enderror
                                     </div>
-
-                                    <div class="form-group">
-                                        <label for="state">State</label>
-                                        <input type="text" name="state" id="state" class="form-control"
-                                            value="{{ old('state', $user->state) }}">
-                                        @error('state')
-                                            <div class="text-danger">{{ $message }}</div>
-                                        @enderror
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label for="city">City</label>
-                                        <input type="text" name="city" id="city" class="form-control"
+<br/>
+<div class="row">
+<div class="col">
+                                            <label for="city">City</label>
+                                            <input type="text" name="city" id="city" class="form-control"
                                             value="{{ old('city', $user->city) }}">
-                                        @error('city')
-                                            <div class="text-danger">{{ $message }}</div>
-                                        @enderror
-                                    </div>
-
-
-                                    <div class="form-group">
-                                        <label for="zip">Zip</label>
-                                        <input type="text" name="zip" id="zip" class="form-control"
+                                            @error('city')
+                                                <div class="text-danger">{{ $message }}</div>
+                                            @enderror
+                                        </div>
+                                        <div class="col">
+                                            <label for="state">State</label>
+                                            <input type="text" name="state" id="state" class="form-control"
+                                            value="{{ old('state', $user->state) }}">
+                                            @error('state')
+                                                <div class="text-danger">{{ $message }}</div>
+                                            @enderror
+                                        </div>
+                                        <div class="col">
+                                            <label for="zip">Zip</label>
+                                            <input type="text" name="zip" id="zip" class="form-control"
                                             value="{{ old('zip', $user->zip) }}">
-                                        @error('zip')
-                                            <div class="text-danger">{{ $message }}</div>
-                                        @enderror
-                                    </div>
-
-                                    <div class="form-group">
+                                            @error('zip')
+                                                <div class="text-danger">{{ $message }}</div>
+                                            @enderror
+                                        </div>
+    </div>
+    <br/>
+    <div class="row">
+                                    <div class="col">
                                         <label for="mobile">Phone Number</label>
                                         <input type="text" name="mobile" id="mobile" class="form-control"
                                             value="{{ old('mobile', $user->mobile) }}">
@@ -153,7 +163,7 @@
                                         @enderror
                                     </div>
 
-                                    <div class="form-group">
+                                    <div class="col">
                                         <label for="website_link">Website Address</label>
                                         <input type="text" name="website_link" id="website_link" class="form-control"
                                             value="{{ old('website_link', $user->website_link) }}">
@@ -161,6 +171,8 @@
                                             <div class="text-danger">{{ $message }}</div>
                                         @enderror
                                     </div>
+    </div>
+    <br/>
 
                                     <div class="form-group">
                                         <label>Time Zone</label>
